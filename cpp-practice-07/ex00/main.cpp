2ce5bc8b3bba6175cb793279c81bb1a02b0892d5
basheer laptop <basheer753@gmail.com>
2023-06-09 22:48:20 +0400

    added subject main

--- cpp-practice-07/ex00/main.cpp
+++ cpp-practice-07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bammar <bammar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 22:35:09 by bammar            #+#    #+#             */
-/*   Updated: 2023/06/09 22:45:44 by bammar           ###   ########.fr       */
+/*   Updated: 2023/06/09 22:48:16 by bammar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,38 @@
 
 int main(void)
 {
-	int x = 1;
-	int y = 2;
-	double d1 = -1.2;
-	double d2 = 10.2;
 
-	::swap(x, y);
-	std::cout << x << "\n";
-	std::cout << y << "\n";	
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+	// int x = 1;
+	// int y = 2;
+	// double d1 = -1.2;
+	// double d2 = 10.2;
 
-	std::cout << "----\n";
+	// ::swap(x, y);
+	// std::cout << x << "\n";
+	// std::cout << y << "\n";	
 
-	std::cout << ::max(x, y) << "\n";
-	std::cout << ::min(x, y) << "\n";
+	// std::cout << "----\n";
 
-	std::cout << "----\n";
+	// std::cout << ::max(x, y) << "\n";
+	// std::cout << ::min(x, y) << "\n";
 
-	std::cout << ::max(d1, d2) << "\n";
-	std::cout << ::min(d1, d2) << "\n";
+	// std::cout << "----\n";
+
+	// std::cout << ::max(d1, d2) << "\n";
+	// std::cout << ::min(d1, d2) << "\n";s
 
 	return 0;
 }
